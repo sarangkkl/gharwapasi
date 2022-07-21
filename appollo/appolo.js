8d6836798b33966cf93d45160e69671688d83993
sarangkkl <sarangkkl2@gmail.com>
2022-07-21 13:28:23 +0530

    j

--- appollo/appolo.js
+++ appollo/appolo.js
@@ -1,6 +1,6 @@
-import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client';
+import { ApolloClient, createHttpLink, InMemoryCache,gql } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
-import {gql} from "@apollo/client";
+
 
 
 
